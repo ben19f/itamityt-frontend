9c60e7bbc7146c860fbdd5713ed30f1cddce414b
ben <2390188@mail.ru>
2026-03-27 20:27:58 +0300

    временно пути для бекенда и лого

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// const API_URL = "https://itamityt.ru/api/public";
-// const REDIRECT_URL = "https://itamityt.ru/rserv";
-const API_URL = "/api/public";
-const REDIRECT_URL = "/rserv";
+const API_URL = "https://dev.itamityt.ru/api/public";
+const REDIRECT_URL = "https://dev.itamityt.ru/rserv";
+// const API_URL = "/api/public";
+// const REDIRECT_URL = "/rserv";
 
 const searchBtn = document.getElementById("search-btn");
 const searchUsernameInput = document.getElementById("search-username");
